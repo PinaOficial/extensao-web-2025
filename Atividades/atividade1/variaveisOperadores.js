b0b8f26464fcb3ccfd0b35a6d5ed0ca177e9a8f9
Diego Pina <142325651+PinaOficial@users.noreply.github.com>
2025-03-07 16:23:38 -0300

    Finalização do Código

---
+++ Atividades/atividade1/variaveisOperadores.js
@@ -0,0 +1,17 @@
+let num1 = 10, num2 = 5;
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+let modulo = num1 % num2;
+let exponenciacao = num1 ** num2;
+let divisaoInteira = Math.floor(num1 / num2);
+
+console.log(`Resultado da soma entre ${num1} e ${num2} é ${soma}`);
+console.log(`Resultado da subtração entre ${num1} e ${num2} é ${subtracao}`);
+console.log(`Resultado da multiplicação entre ${num1} e ${num2} é ${multiplicacao}`);
+console.log(`Resultado da divisão entre ${num1} e ${num2} é ${divisao}`);
+console.log(`Resultado do módulo entre ${num1} e ${num2} é ${modulo}`);
+console.log(`Resultado da exponenciação entre ${num1} e ${num2} é ${exponenciacao}`);
+console.log(`Resultado da divisão inteira entre ${num1} e ${num2} é ${divisaoInteira}`);
